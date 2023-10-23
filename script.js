6413af1da649724c50f9b2e875f5f2f6a188a25e
randomu3 <demiz99@mail.ru>
2023-10-23 11:17:20 +0500

    added correect mistakes

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 const chatBox = document.getElementById('chat-box');
+const userInput = document.getElementById('user-input');
 
 function sendMessage() {
-    const userInput = document.getElementById('user-input');
-    const message = userInput.value;
+    const message = userInput.value.trim();  // Удалить пробелы по краям
     userInput.value = '';
     
-    if (message) {
-        const userMessage = document.createElement('div');
-        userMessage.textContent = 'Вы: ' + message;
-        userMessage.className = 'user-message';  /* применить класс user-message */
-        chatBox.appendChild(userMessage);
-        
-        const botResponse = document.createElement('div');
-        botResponse.textContent = getBotResponse(message);
-        botResponse.className = 'bot-message';  /* применить класс bot-message */
-        chatBox.appendChild(botResponse);
+    if (!message) {
+        console.error('The message cannot be empty');  // Логирование ошибки в консоль
+        return;  // Прекратить выполнение функции, если сообщение пустое
     }
+
+    appendMessage('Вы: ' + message, 'user-message');
+    appendMessage(getBotResponse(message), 'bot-message');
+}
+
+function appendMessage(text, className) {
+    const messageDiv = document.createElement('div');
+    messageDiv.textContent = text;
+    messageDiv.className = className;
+    chatBox.appendChild(messageDiv);
 }
 
 function getBotResponse(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  if (lowerCaseMessage.includes('пенсия')) {
-    return 'AI-Помощник: Мы предлагаем различные пенсионные программы...';
-  } else if (lowerCaseMessage.includes('вклад')) {
-    return 'AI-Помощник: У нас есть множество вариантов вкладов...';
-  } else {
-    return 'AI-Помощник: Извините, я не понял ваш вопрос.';
-  }
+    const lowerCaseMessage = message.toLowerCase();
+    if (lowerCaseMessage.includes('пенсия')) {
+        return 'AI-Помощник: Мы предлагаем различные пенсионные программы...';
+    } else if (lowerCaseMessage.includes('вклад')) {
+        return 'AI-Помощник: У нас есть множество вариантов вкладов...';
+    } else {
+        return 'AI-Помощник: Извините, я не понял ваш вопрос.';
+    }
 }
